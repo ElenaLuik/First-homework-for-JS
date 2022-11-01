28e764fc41016dd4bc360b1ac14555ea249cb3cc
Elena Luyk <111731239+ElenaLuik@users.noreply.github.com>
2022-11-01 20:26:17 +0300

    Первое дз по JS

---
+++ variables.js
@@ -0,0 +1,13 @@
+let yourAge = 32;
+let ageBestFriend = 26;
+let yourYearOfBirth = 1990;
+let thisYear = 2022;
+let howMuchIsADollarInRubles = 61;
+
+const yourName = 'Елена';
+const nameBestFriend = 'Анна';
+const yourCity = 'Санкт-Петербург';
+const capitalOfYourCountry = 'Москва';
+const descriptionOfYouInOneWord = 'злюка';
+
+console.log(`Меня зовут ${yourName}, мне ${yourAge} года. Я из города ${yourCity} и я ${descriptionOfYouInOneWord}`);
